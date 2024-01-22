3071ee63241eaa65f762d75ce0681407546f8d78
Renat <renat_samigullin_97@mail.ru>
2024-01-22 23:31:02 +0500

    Добавление возможности чтения содержимого файла конфигурации

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Загрузка информации их файла конфигурации 
+
+var configuration = app.Configuration;
+
+var config = configuration["CustomGreetings"];
+
+app.MapGet("/", () => config);
 
 app.Run();

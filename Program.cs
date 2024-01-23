f16c47d9cfda893c91d3df1e2bde0da85a2e7bc3
Renat <renat_samigullin_97@mail.ru>
2024-01-23 20:31:20 +0500

    Добавление первого и главного контроллера

--- Program.cs
+++ Program.cs
@@ -1,12 +1,27 @@
 var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+services.AddControllersWithViews(); // добавляем структуру MVC
+
 var app = builder.Build();
 
-// Загрузка информации их файла конфигурации 
+if (app.Environment.IsDevelopment()) // если приложение находится в состоянии разработки
+{
+    app.UseDeveloperExceptionPage(); // добавляем промежуточное ПО - перехватывает все исключения
+    // визуализирует ошибки на страничке в браузере
+}
 
+app.UseRouting(); // добавляем систему маршутизации
+
+// Загрузка информации их файла конфигурации
 // var configuration = app.Configuration;
-
 // var config = configuration["CustomGreetings"]; // считывает и все
 
-app.MapGet("/", () => app.Configuration["CustomGreetings"]); // в реальном времени выводит актуальную информацию
+// app.MapGet("/", () => app.Configuration["CustomGreetings"]); // маршрут - в реальном времени выводит актуальную информацию - перехватывает все обращения к корню сайта
+app.MapGet("/throw", () => { 
+    throw new ApplicationException("Ошибка в программе"); 
+});
+
+app.MapDefaultControllerRoute(); // добавляем обработку входящих подключений системы MVC
 
 app.Run();
